0ee8267b3e9d34d72bec3ac1fa89f992039463d6
rosana-moreira <rooh.moreiraa@gmail.com>
2023-05-10 16:18:01 -0300

    Esduto com Union all

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,5 +1,7 @@
 package com.devsuperior.uri2737;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,19 +9,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
 
-	@Autowired
-	private LawyerRepository repository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Uri2737Application.class, args);
-	}
+    @Autowired
+    private LawyerRepository repository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2737Application.class, args);
+    }
 
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        List<LawyerMinProjection> list = repository.search1();
+        List<LawyerMinDTO> result1 = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+
+        System.out.println("\n*** Resultado Sql Raiz: ");
+        for (LawyerMinDTO obj : result1) {
+            System.out.println(obj);
+        }
+
+    }
 }
--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,29 @@
 package com.devsuperior.uri2737.repositories;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
+    @Query(nativeQuery = true, value = "(select name, customers_number  as customersNumber" +
+            "from lawyers " +
+            "order by customers_number " +
+            "desc limit 1) " +
+            "union all " +
+            "(select name, customers_number " +
+            "from lawyers  " +
+            "order by customers_number  " +
+            "asc limit 1) " +
+            "union all " +
+            "(select 'Average', round(avg(customers_number)) " +
+            "from lawyers)")
+    List<LawyerMinProjection> search1();
 
 }
